39f65219ed9fda6368271aa2745321da34230ba5
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-08-20 16:37:35 +0000

    install react-slick and slick-carousel for building the slideshow

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -3,7 +3,16 @@ import React from 'react';
 function Categories() {
   return (
     <div className="categories" data-testid="categories">
-      <h1>Categories</h1>
+      <h2>Categories</h2>
+      <div className="home">
+        <h3>Home</h3>
+      </div>
+      <div className="clothes">
+        <h3>Clothes</h3>
+      </div>
+      <div className="car">
+        <h3>Car</h3>
+      </div>
     </div>
   );
 }
