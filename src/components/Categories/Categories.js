620535fb373c98c3656805ceed4575886a360dd0
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-08-21 21:59:57 +0000

    Create the Category component

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,18 +1,14 @@
 import React from 'react';
+import Category from '../Category/Category';
 
 function Categories() {
   return (
     <div className="categories" data-testid="categories">
       <h2>Categories</h2>
-      <div className="home">
-        <h3>Home</h3>
-      </div>
-      <div className="clothes">
-        <h3>Clothes</h3>
-      </div>
-      <div className="car">
-        <h3>Car</h3>
-      </div>
+      <Category category="home" />
+      <Category category="jewelry" />
+      <Category category="car" />
+      <Category category="clothes" />
     </div>
   );
 }
---
+++ src/components/Category/Category.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Category.css';
+
+function Category({ category }) {
+  return (
+    <div>
+      <h3>{category}</h3>
+    </div>
+  );
+}
+
+export default Category;
