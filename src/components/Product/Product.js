879ff68e048fe0501056cbd4e39c1a563f2baeb7
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-08-26 16:01:58 +0000

    Insert the content for showing all products

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import './Product.css';
 
 function Product(props) {
   const { name, description, price, picture } = props.product;
   return (
-    <div className="product">
-      <img src={picture.url} alt={name} />
-      <h2>{name}</h2>
-      <p>{description}</p>
+    <div className="row" id="ads">
+      <div>
+        <div className="card rounded">
+          <div className="card-image">
+            <span className="card-notify-badge">Low KMS</span>
+            <span className="card-notify-year">2018</span>
+            <img className="img-fluid" src={picture.url} alt={name} />
+          </div>
+          <div className="card-image-overlay m-auto">
+            <span className="card-detail-badge">Used</span>
+            <span className="card-detail-badge">$28,000.00</span>
+            <span className="card-detail-badge">13000 Kms</span>
+          </div>
+          <div className="card-body text-center">
+            <div className="ad-title m-auto">
+              <h5>{name}</h5>
+            </div>
+            <a className="ad-btn" href="#">
+              View
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
