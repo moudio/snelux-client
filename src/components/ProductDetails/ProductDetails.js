1a3225b467d31a5ad1a582e8c8715a43987884cb
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-08-27 20:23:48 +0000

    Create the product view first content

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -7,17 +7,25 @@ import loading from '../../pictures/loading.gif';
 function ProductDetails({ userState, productsState }) {
   const { products } = productsState;
   const { name } = useParams();
-  if (products) {
-    let product = products.find((product) => product.name === name);
-
-    return (
-      <div className="container">
-        <h1>Details for {product.name}</h1>
-      </div>
-    );
-  } else {
+  if (!products) {
     return <img className="loading-product-view" src={loading} alt="" />;
   }
+
+  let product = products.find((product) => product.name === name);
+
+  const { description, price, picture } = product;
+  return (
+    <div className="container">
+      <h1>Details for {product.name}</h1>
+      <div className="product-image-and-content">
+        <div className="product-content">
+          <p>{description}</p>
+          <p>{price}</p>
+          <img src={picture.url} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 const mapStateToProps = (state) => ({
   userState: state.userReducer,
